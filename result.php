fdd8257fd877b3a4453310687f351664775b7072
Xiaoming Cai(Ming) <ming.cxm@gmail.com>
2012-11-03 18:35:02 +0100

    send coordinates instead of location names in the query string to google map, this solves the character encoding problem; some other minor improvements

--- result.php
+++ result.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	session_start();
 	include_once ("settings.php");
 	include_once (SERVER_ROOT. "/class/DbConnection.php");
@@ -8,16 +8,16 @@
 	include_once (SERVER_ROOT. "/class/Calculator.php");
 	include_once (SERVER_ROOT. "/class/Location.php");
 	include_once (SERVER_ROOT."/include/header.php");
-	
-	
+
+
 ?>
  <script type="text/javascript">
-  
+
     var geocoder, location1, location2;
 
 	function initialize() {
-		
-		<?php 
+
+		<?php
 			if (isset($_SESSION['rest'])){
 				//var_dump($_SESSION['from']);die;
 				$restaurant = unserialize($_SESSION['rest']);
@@ -37,55 +37,39 @@
 	{
 		try
 		{
-			
+
 			var glatlng1 = new GLatLng(location1_lat, location1_lon);
 			var glatlng2 = new GLatLng(location2_lat, location2_lon);
 			//var glatlng1 = new GLatLng(location1_lon, location1_lat);
 			//var glatlng2 = new GLatLng(location2_lon, location2_lat);
-			
+
 			var miledistance = glatlng1.distanceFrom(glatlng2, 3959).toFixed(1);
 			var kmdistance = (miledistance * 1.609344).toFixed(1);
+
+			var defaultTravelMode = "walking";
+			var defaultLocale = "sv_SE";
 
 			 map = new GMap2(document.getElementById("map_canvas"));
 			 map.setCenter(glatlng2, 13);
 
 			 directions = new GDirections(map, null);
-		/*	 GEventListener listener = GEvent.addListener(directions, 'load',new 
+		/*	 GEventListener listener = GEvent.addListener(directions, 'load',new
 					 GEventHandler(){
 				//using prototype, u can use any other to attach event
 				alert(directions.getDistance().meters);
 				//display results
 				});
 		*/
-			//var gString = 'from: ' + location1_lon +',' + location1_lat + ' to:' + location2_lon +',' + location2_lat;
-			 
-			 var gString = 'from: <?=$_SESSION['location']?>, sweden to:<?=(isset($restaurant))? $restaurant->getStreet(): ''?>, sweden';
-			
-			directions.load(gString);
+			var gString = 'from: ' + location1_lon +',' + location1_lat + ' to:' + location2_lon +',' + location2_lat;
 
-			 		 
-			
-	/*		 var point1 = glatlng1;
-			 var point2 = glatlng2;
-			 var marker1 = new GMarker(point1);
-			 var marker2 = new GMarker(point2);
+//			 var gString = 'from: <?=$_SESSION['location']?>, sweden to:<?=(isset($restaurant))? $restaurant->getStreet(): ''?>, sweden';
 
-			 map.addOverlay(marker1);
-			 map.addOverlay(marker2);
+			 var queryOptions = {"locale": defaultLocale, "travelMode": defaultTravelMode};
 
-			 
-			 
+			directions.load(gString, queryOptions);
 
-			 GEvent.addListener(marker1, "click", function() {
-				    marker1.openInfoWindowHtml("Here is where you are.");
-				});
-
-			 GEvent.addListener(marker2, "click", function() {
-				    marker2.openInfoWindowHtml("The restaurant is here.");
-				});
-	*/
 			 map.setUIToDefault();
-			
+
 		}
 		catch (error)
 		{
@@ -94,25 +78,15 @@
 	}
 
     </script>
-<?php 
-	function getDistance($dist) {
-var_dump($dist);
-		$newDist = explode('.',$dist);
-		$newDist = preg_match("/^([0-9]{3})/",$newDist[1], $saveDist);
-die(var_dump($saveDist[0]));
-		return $saveDist[0];
-	}
-	
-?>
-	
+
 	<body onload="initialize()">
-	
+
 		<div id="top">
 			<div class="center">
 				<?php
 					if(isset($restaurant)) {
 				?>
-				
+
 				<div>
 					<a href="prototype.php"><img id="confused_smiley" src="image/site/smiley_blue_happy.png" alt="confused" /></a>
 					<div id="front">
@@ -136,7 +110,7 @@ die(var_dump($saveDist[0]));
 							<tr>
 								<td class="header">Web</td><td><?php echo $site ?></td>
 							</tr>
-							<? endif;?>							
+							<? endif;?>
 							</table>
 							<br />
 							<br />
@@ -162,8 +136,8 @@ die(var_dump($saveDist[0]));
 		<div id="bottom">
 			<div class="center">
 				<p class="map_result">The map shows you how to drive to the restaurant.</p>
-				<p class="map_result">From where you are now you have exactly<span class="distinguish"> <?php echo $restaurant->getDistance() ?> </span>Km to this restaurant.</p>
-				<div id="map_canvas" style="width: 500px;height:400px;"></div>
+				<p class="map_result">From where you are now you have exactly<span class="distinguish"> <?php echo $restaurant->getDistanceInMeters() ?> </span>m to this restaurant.</p>
+				<div id="map_canvas" style="width: 800px;height:600px;"></div>
 			</div>
 		</div>
 
